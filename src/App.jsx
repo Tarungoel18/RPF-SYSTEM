ac8cacff4a8813640e5036bf9c78707166bb6f15
Tarun Goel <tarungoel1823@gmail.com>
2026-02-10 16:02:08 +0530

    Changed the css order

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./assets/css/app.css";
-import "./assets/css/app.min.css";
-import "./assets/css/bootstrap.css";
 import "./assets/css/bootstrap.min.css";
-import "./assets/css/icons.css";
+import "./assets/css/bootstrap.css";
 import "./assets/css/icons.min.css";
+import "./assets/css/icons.css";
+import "./assets/css/app.min.css";
+import "./assets/css/app.css";
 import "./assets/css/style.css";
 import { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
